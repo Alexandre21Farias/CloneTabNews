b83eb2fb7f18a73804bf5f21529400e93773f958
Alexandre Lima Farias <Alexandre21farias@gmail.com>
2025-12-02 12:12:07 -0300

    add script for formatation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <h1>EU TE AMO MINHA MONDRONGUINHA</h1>
+  return <h1>EU TE AMO MINHA MONDRONGUINHA</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
