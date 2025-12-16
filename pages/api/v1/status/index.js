d2e6d092c97c6ccd2c94ae4b2ab7ba21f78f7ae9
ALEXANDRE LIMA FARIAS <alexandre21farias@gmail.com>
2025-12-16 21:03:38 +0000

    refactor: update database connection and remove unused files
    - Moved database configuration to .env file for better security.
    - Implemented database query function in database.js.
    - Removed calculadora model and its tests as they are no longer needed.
    - Updated package.json and package-lock.json to include pg dependency.
    - Added API status endpoint to check database connectivity.
    - Cleaned up unused API status file and tests.

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,7 @@
+import database from "../../../../infra/database.js"
+
+export default async function status(request, response){
+    const result = await database.query("SELECT 1 + 1 as sum;");
+    console.log(result.rows)
+    response.status(200).send("all good");
+}
