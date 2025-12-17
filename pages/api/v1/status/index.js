b858810adbc3e8b15bf86278c560002ddc4dfa6d
ALEXANDRE LIMA FARIAS <alexandre21farias@gmail.com>
2025-12-17 23:25:27 +0000

    salvar isso aqui pra nao perder nem dar bo

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,27 @@
 import database from "infra/database.js"
 
 export default async function status(request, response){
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log(result.rows)
-    response.status(200).send("all good");
+    const updateAt = new Date().toISOString();
+
+    const databaseVersion = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersion.rows[0].server_version;
+
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+    const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+    const databaseName = "local_db"
+    const databaseConnectionsResult = await database.query(`SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';`);
+    const databaseConnectionsValue = databaseConnectionsResult.rows.length;
+    response.status(200).json({
+        update_at: updateAt,
+        dependencies:{
+            version: databaseVersionValue,
+            max_connections: databaseMaxConnectionsValue,
+            database_connections: databaseConnectionsValue
+        }
+    });
+}
+
+function getDatabaseVersion(){
+
 }
