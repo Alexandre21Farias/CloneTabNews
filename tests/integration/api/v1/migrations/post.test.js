fcb6db64e46a9b9a523643ac50e79fe8789514a6
Alexandre Lima Farias <Alexandre21farias@gmail.com>
2026-07-21 18:31:25 -0300

    fix: resolve orchestrator migrator import error and apply prettier formatting

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -10,12 +10,9 @@ describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
-        const response1 = await fetch(
-          `${webserver.origin}/api/v1/migrations`,
-          {
-            method: "POST",
-          },
-        );
+        const response1 = await fetch(`${webserver.origin}/api/v1/migrations`, {
+          method: "POST",
+        });
         expect(response1.status).toBe(201);
 
         const response1Body = await response1.json();
@@ -24,12 +21,9 @@ describe("POST /api/v1/migrations", () => {
         expect(response1Body.length).toBeGreaterThan(0);
       });
       test("For the second time", async () => {
-        const response2 = await fetch(
-          `${webserver.origin}/api/v1/migrations`,
-          {
-            method: "POST",
-          },
-        );
+        const response2 = await fetch(`${webserver.origin}/api/v1/migrations`, {
+          method: "POST",
+        });
         expect(response2.status).toBe(200);
 
         const response2Body = await response2.json();
