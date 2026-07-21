404cce733f4f48ffcbdf70bc9d5339aba5fecaa1
Alexandre Lima Farias <Alexandre21farias@gmail.com>
2026-07-20 21:57:36 -0300

    refactor: remove unnecessary newlines in various files

--- tests/integration/users/post.test.js
+++ tests/integration/users/post.test.js
@@ -7,15 +7,11 @@ beforeAll(async () => {
 
 describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
-      test("With unique and valid data", async () => {
-        const response = await fetch(
-          "http://localhost:3000/api/v1/users",
-          {
-            method: "POST",
-          },
-        );
-        expect(response.status).toBe(201);
+    test("With unique and valid data", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
       });
-    
+      expect(response.status).toBe(201);
+    });
   });
 });
